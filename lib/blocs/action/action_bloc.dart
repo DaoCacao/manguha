684232735d3b59f264498234527c4b01c888b44d
Дмитрий Родионов <MrDaoCacao@gmail.com>
2020-10-19 19:44:49 +0300

    save note  as txt

--- lib/blocs/action/action_bloc.dart
+++ lib/blocs/action/action_bloc.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:manguha/data/note.dart';
 import 'package:manguha/data/note_repository.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'action_state.dart';
 
@@ -59,9 +64,12 @@ class ActionCubit extends Cubit<ActionState> {
     _notes.save(notes);
   }
 
-  void download(List<Note> notes, String type) {
+  void download(List<Note> notes, String type) async {
+    //FIXME cant find saved file
     emit(ActionState.Default);
-    //TODO add download
+
+    if (await Permission.storage.request().isGranted)
+      notes.forEach((note) => _saveNote(note, type));
   }
 
   void delete(List<Note> notes) {
@@ -91,5 +99,13 @@ class ActionCubit extends Cubit<ActionState> {
   void fullDelete(List<Note> notes) {
     emit(ActionState.Default);
     _notes.fullDelete(notes);
+  }
+
+  Future _saveNote(Note note, String type) {
+    getExternalStorageDirectory()
+        .then((dir) => dir.path)
+        .then((path) => join(path, "${note.title}.$type"))
+        .then((path) => File(path))
+        .then((file) => file.writeAsString("${note.title}\n${note.content}"));
   }
 }
